29b37d0d7429e4b8f6a73837df8f3704782fb537
Benson Mwangi <kimaniben@gmail.com>
2023-11-30 05:02:38 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Todo from './Components/Todo'
+import TodoList from './Components/TodoList';
 
 function App() {
+  const todos = [
+    { id: 1, text: 'Learn React', completed: false },
+    { id: 2, text: 'Build a Todo App', completed: true },
+    // Add more todos as needed
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <TodoList todos={todos} />
     </div>
   );
 }
